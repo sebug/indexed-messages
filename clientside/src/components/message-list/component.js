d0810ed3686028cf5520c6b1a2c3714d03d8f853
sebug <sebastian.gfeller@gmail.com>
2018-10-15 15:22:09 +0200

    With polyfill reference

--- clientside/src/components/message-list/component.js
+++ clientside/src/components/message-list/component.js
@@ -9,6 +9,18 @@ class ViewModel {
 	}
 
 	console.log('The key in the sub component is ' + this.key());
+
+	this.getData = this.getData.bind(this);
+
+	setTimeout(() => {
+	    this.getData();
+	}, 0);
+    }
+
+    async getData() {
+	let response = await fetch('/api/GetMessagesTrigger?code=' + this.key());
+	let responseModel = await response.json();
+	console.log(responseModel);
     }
 }
 
