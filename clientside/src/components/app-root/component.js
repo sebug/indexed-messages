61a6a0cb47823f5485598dae083e25eada4d0818
sebug <sebastian.gfeller@gmail.com>
2018-10-15 14:54:58 +0200

    Adding reference to API key so we can make function calls

--- clientside/src/components/app-root/component.js
+++ clientside/src/components/app-root/component.js
@@ -3,7 +3,9 @@ import htmlContent from './component.html';
 
 class ViewModel {
     constructor(params) {
-	
+	let sps = (new URL(document.location)).searchParams;
+	let key = sps.get("key");
+	this.key = ko.observable(key);
     }
 }
 
