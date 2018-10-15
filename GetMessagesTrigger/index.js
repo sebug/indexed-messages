bccc01ec8dd2a0d0a55015bb78c48c5dc5f90701
sebug <sebastian.gfeller@gmail.com>
2018-10-15 15:58:08 +0200

    Adding table service queries

--- GetMessagesTrigger/index.js
+++ GetMessagesTrigger/index.js
@@ -1,10 +1,26 @@
 let azure = require('azure-storage');
 
 module.exports = function (context, req) {
-    context.res = {
-	body: {
-	    msg: "Hey from JSON"
+    let tableService = azure.createTableService();
+    let query = new azure.TableQuery()
+	.top(100)
+	.where('PartitionKey eq ?', 'prod');
+
+    tableService.queryEntities('mytable', query, null, function(error, result, response) {
+	if (!error) {
+	    context.log('Got something back');
+	    context.log(JSON.stringify(result.entries));
+	    context.res = {
+		body: {
+		    msg: "Hey from JSON"
+		}
+	    };
+	    context.done();
+	} else {
+	    context.res = {
+		body: []
+	    };
+	    context.done();
 	}
-    };
-    context.done();
+    });
 };
