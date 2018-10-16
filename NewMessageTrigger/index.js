b2246468db46b79e4e08119e0bf6fe8cc219c909
sebug <sebastian.gfeller@gmail.com>
2018-10-16 09:32:48 +0200

    Trying to insert entity

--- NewMessageTrigger/index.js
+++ NewMessageTrigger/index.js
@@ -2,10 +2,27 @@ let azure = require('azure-storage');
 
 module.exports = function (context, req) {
     let tableService = azure.createTableService();
+    let entGen = azure.TableUtilities.entityGenerator;
     context.log(JSON.stringify(req.body));
-
-    context.res = {
-	body: req.body
+    let entity = {
+	partitionKey: entGen.String('prod'),
+	RowKey: entGen.String(req.body.dateTime),
+	From: entGen.String(req.body.from),
+	To: entGen.String(req.body.to),
+	Message: entGen.String(req.body.message)
     };
-    context.done();
+    tableService.insertEntity('radioMessages', entity, function(error, result, response) {
+	if (!error) {
+	    context.log(JSON.stringify(result));
+	    context.res = {
+		body: req.body
+	    };
+	    context.done();
+	} else {
+	    context.res = {
+		body: "Error"
+	    };
+	    context.done();
+	}
+});
 };
