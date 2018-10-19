d8527b53e9f35e2a9b210a2d1423871f818d6ba7
sebug <sebastian.gfeller@gmail.com>
2018-10-19 11:20:17 +0200

    Special casing CPA now that we know that partitions work

--- clientside/serviceworker/index.js
+++ clientside/serviceworker/index.js
@@ -1,8 +1,8 @@
 // The service worker to be used for this sub-element.
 import idb from 'idb';
 
-var CACHE_NAME = 'my-static-site-cache-v1.49';
-var DYNAMIC_CACHE_NAME = 'my-dynamic-site-cache-1.49';
+var CACHE_NAME = 'my-static-site-cache-v1.50';
+var DYNAMIC_CACHE_NAME = 'my-dynamic-site-cache-1.50';
 var urlsToCache = [
   '/',
   '/polyfill.min.js',
@@ -36,7 +36,7 @@ self.addEventListener('install', function (e) {
     try {
 	// Delete old caches
 	let i;
-	for (i = 0; i < 49; i += 1) {
+	for (i = 0; i < 50; i += 1) {
 	    let cacheKey = 'my-static-site-cache-v1.' + i;
 	    caches.delete(cacheKey);
 	    let dynamicCacheKey = 'my-dynamic-site-cache-1.' + i;
