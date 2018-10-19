9fce1e13ce875989488426c453b44177071eede4
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2018-10-19 13:42:17 +0200

    Bump cache version

--- clientside/serviceworker/index.js
+++ clientside/serviceworker/index.js
@@ -1,8 +1,8 @@
 // The service worker to be used for this sub-element.
 import idb from 'idb';
 
-var CACHE_NAME = 'my-static-site-cache-v1.54';
-var DYNAMIC_CACHE_NAME = 'my-dynamic-site-cache-1.54';
+var CACHE_NAME = 'my-static-site-cache-v1.55';
+var DYNAMIC_CACHE_NAME = 'my-dynamic-site-cache-1.55';
 var urlsToCache = [
   '/',
   '/polyfill.min.js',
@@ -36,7 +36,7 @@ self.addEventListener('install', function (e) {
     try {
 	// Delete old caches
 	let i;
-	for (i = 0; i < 54; i += 1) {
+	for (i = 0; i < 55; i += 1) {
 	    let cacheKey = 'my-static-site-cache-v1.' + i;
 	    caches.delete(cacheKey);
 	    let dynamicCacheKey = 'my-dynamic-site-cache-1.' + i;
