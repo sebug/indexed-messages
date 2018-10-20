af2f17cda271b371e8168d64675c82bd6e3da805
sebug <sebastian.gfeller@gmail.com>
2018-10-20 16:41:01 +0200

    Creating hashes

--- pushnotifications/index.js
+++ pushnotifications/index.js
@@ -1,28 +1,41 @@
 const sharp = require('sharp');
+const crypto = require('crypto');
+const fs = require('fs-extra');
 
 const sizes = [16, 32, 128];
+const sizesWithMultiplier = sizes.map(size => {
+    return {
+	size: size,
+	times: 1
+    };
+}).concat(sizes.map(size => {
+    return {
+	size: size,
+	times: 2
+    };
+}));
 
 // create icons for push notification
-sizes.forEach(size => {
-    sharp('../clientside/images/logo_messages.png')
-	.resize(size, size)
-	.toFile('dist/IndexedMessages.pushpackage/icon.iconset/icon_' + size +
-		'x' + size + '.png', (err, info) => {
-		    if (err) {
-			console.log(JSON.stringify(err));
-		    } else {
-			console.log('created push notification logo with width ' + info.width);
-		    }
-		});
-
-    sharp('../clientside/images/logo_messages.png')
-	.resize(size * 2, size * 2)
-	.toFile('dist/IndexedMessages.pushpackage/icon.iconset/icon_' + size +
-		'x' + size + '@2x.png', (err, info) => {
-		    if (err) {
-			console.log(JSON.stringify(err));
-		    } else {
-			console.log('created 2x push notification logo with width ' + info.width);
-		    }
-		});
+const filePrefix = 'dist/IndexedMessages.pushpackage/';
+Promise.all(sizesWithMultiplier.map(o => {
+    return sharp('../clientside/images/logo_messages.png')
+	.resize(o.size * o.times, o.size * o.times)
+        .toBuffer()
+        .then(resizedImageBuffer => {
+	    const sha512Sum = crypto.createHash('sha512');
+	    sha512Sum.update(resizedImageBuffer);
+	    const digest = sha512Sum.digest('hex');
+	    const fileNamePart = 'icon.iconset/icon_' + o.size +
+		'x' + o.size + (o.times === 2 ? '@2x': '') + '.png';
+	    const fileName = filePrefix + fileNamePart;
+	    return fs.outputFile(fileName, resizedImageBuffer).then(() => {
+		return {
+		    fileName: fileNamePart,
+		    hashType: 'sha512',
+		    hashValue: digest
+		};
+	    });
+	});
+})).then(filesWithDigest => {
+    console.log(filesWithDigest);
 });
