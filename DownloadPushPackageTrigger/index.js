8500dee563657fead4a169d7ec52d5a47a21bacd
sebug <sebastian.gfeller@gmail.com>
2018-10-21 15:28:21 +0200

    Using azure-storage instead

--- DownloadPushPackageTrigger/index.js
+++ DownloadPushPackageTrigger/index.js
@@ -1,50 +1,29 @@
-const https = require('https');
+const azure = require('azure-storage');
 
 module.exports = function (context, req) {
     context.log('body is ' + JSON.stringify(req.body || {}));
     context.log('version is ' + req.query.version);
     context.log('website push ID is ' + req.query.websitePushID);
-    context.log('starting zip request');
-
-    try {
-	const options = {
-	    timeout: 10 * 1000
-	};
-	const zipRequest = https.request('https://indexedmessages.blob.core.windows.net/indexedmessagesstatic/package.zip', options, (res) => {
-	    var body = "";
-	    context.log('getting res');
-	    
-	    res.on("data", (chunk) => {
-		context.log('got data');
-		body += chunk;
-	    });
-
-	    res.on("end", () => {
-		context.log('ending request');
-		context.res = {
-		    body: body,
-		    status: 200,
-		    headers: {
-			'Content-Type': 'application/zip'
-		    }
-		};
-		context.done();
-	    });
-	}).on('error', (err) => {
+    const blobService = azure.createBlobService();
+    blobService.getBlobToText('indexedmessagesstatic','package.zip', (err, data) => {
+	if (err) {
 	    context.log('error');
-	    context.log(JSON.stringify(err));
+	    context.log(err);
 	    context.res = {
 		status: 500,
 		body: err.message
 	    };
 	    context.done();
-	});
-    } catch (e) {
-	context.log(e);
-	context.res = {
-		status: 500,
-		body: 'an error occurred'
-	};
-	context.done();
-    }
+	} else {
+	    context.log('Got data');
+		context.res = {
+		    body: data,
+		    status: 200,
+		    headers: {
+			'Content-Type': 'application/zip'
+		    }
+		};
+	    context.done();
+	}
+    });
 };
