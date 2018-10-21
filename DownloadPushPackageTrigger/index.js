6c7cd83f4ffb09213005aec51f86223b4c524192
sebug <sebastian.gfeller@gmail.com>
2018-10-21 14:45:46 +0200

    Returning zip

--- DownloadPushPackageTrigger/index.js
+++ DownloadPushPackageTrigger/index.js
@@ -1,13 +1,27 @@
+const https = require('https');
 
-module.exports = function (context, req, blobContents) {
+module.exports = function (context, req) {
     context.log('body is ' + JSON.stringify(req.body));
     context.log('version is ' + req.query.version);
     context.log('website push ID is ' + req.query.websitePushID);
     context.log('blob contents are');
-    context.log(blobContents);
 
-    context.res = {
-	body: 'Hello from get push package'
-    };
-    context.done();
+    const req = http.request('https://indexedmessages.azurewebsites.net/static/package.zip', (res) => {
+	var body = "";
+
+	res.on("data", (chunk) => {
+	    body += chunk;
+	});
+
+	res.on("end", () => {
+	    context.res = {
+		body: body,
+		status: 200,
+		headers: {
+		    'Content-Type': 'application/zip'
+		}
+	    };
+	    context.done();
+	});
+    });
 };
