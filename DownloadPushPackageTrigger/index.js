e5a30d5003552027b480146c5c52a34b31e31337
sebug <sebastian.gfeller@gmail.com>
2018-10-21 14:35:45 +0200

    Blob contents input

--- DownloadPushPackageTrigger/index.js
+++ DownloadPushPackageTrigger/index.js
@@ -1,8 +1,10 @@
 
-module.exports = function (context, req) {
+module.exports = function (context, req, blobContents) {
     context.log('body is ' + JSON.stringify(req.body));
     context.log('version is ' + req.query.version);
     context.log('website push ID is ' + req.query.websitePushID);
+    context.log('blob contents are');
+    context.log(blobContents);
 
     context.res = {
 	body: 'Hello from get push package'
