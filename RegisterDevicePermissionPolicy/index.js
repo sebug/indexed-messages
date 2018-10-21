f4a070b7024e1e5688bb033b7fe556ab17c821c5
sebug <sebastian.gfeller@gmail.com>
2018-10-21 18:45:39 +0200

    Disambiguating register device permission

--- RegisterDevicePermissionPolicy/index.js
+++ RegisterDevicePermissionPolicy/index.js
@@ -12,25 +12,48 @@ module.exports = function (context, req) {
     const tableService = azure.createTableService();
     const entGen = azure.TableUtilities.entityGenerator;
 
-    const entity = {
-	PartitionKey: entGen.String('prod'),
-	RowKey: entGen.String(deviceToken),
-	WebsitePushID: entGen.String(websitePushID)
-    };
+    if (req.method.toLowerCase() === 'delete') {
+	var task = {
+	    PartitionKey: {'_':'prod'},
+	    RowKey: {'_': deviceToken}
+	};
 
-    tableService.insertOrReplaceEntity('registeredDevices', entity, (error, result, response) => {
-	if (!error) {
-	    context.res = {
-		body: 'OK'
-	    };
-	    context.done();
-	} else {
-	    context.log(JSON.stringify(error));
-	    context.res = {
-		status: 500,
-		body: 'Error inserting device registration'
-	    };
-	    context.done();
-	}
-    });
+	tableSvc.deleteEntity('registeredDevices', task, function(error, response){
+	    if (!error) {
+		context.res = {
+		    body: 'OK'
+		};
+		context.done();
+	    } else {
+		context.log(JSON.stringify(error));
+		context.res = {
+		    status: 500,
+		    body: 'Error forgetting device registration'
+		};
+		context.done();
+	    }
+	});
+    } else {
+	const entity = {
+	    PartitionKey: entGen.String('prod'),
+	    RowKey: entGen.String(deviceToken),
+	    WebsitePushID: entGen.String(websitePushID)
+	};
+
+	tableService.insertOrReplaceEntity('registeredDevices', entity, (error, result, response) => {
+	    if (!error) {
+		context.res = {
+		    body: 'OK'
+		};
+		context.done();
+	    } else {
+		context.log(JSON.stringify(error));
+		context.res = {
+		    status: 500,
+		    body: 'Error inserting device registration'
+		};
+		context.done();
+	    }
+	});
+    }
 };
