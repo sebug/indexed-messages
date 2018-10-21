4e47deb456c53c3932061fb2b31922c5d93bb75e
sebug <sebastian.gfeller@gmail.com>
2018-10-21 15:02:07 +0200

    Downloading from blob.core

--- DownloadPushPackageTrigger/index.js
+++ DownloadPushPackageTrigger/index.js
@@ -6,7 +6,7 @@ module.exports = function (context, req) {
     context.log('website push ID is ' + req.query.websitePushID);
     context.log('starting zip request');
 
-    const zipRequest = https.request('https://indexedmessages.azurewebsites.net/static/package.zip', (res) => {
+    const zipRequest = https.request('https://indexedmessages.blob.core.windows.net/indexedmessagesstatic/package.zip', (res) => {
 	var body = "";
 
 	res.on("data", (chunk) => {
