88ebe9b9442ed2a6eef1c6d2afb2e14942543d45
sebug <sebastian.gfeller@gmail.com>
2018-10-21 15:42:43 +0200

    Trying with a writable stream

--- DownloadPushPackageTrigger/index.js
+++ DownloadPushPackageTrigger/index.js
@@ -1,11 +1,26 @@
 const azure = require('azure-storage');
+const stream = require('stream');
 
 module.exports = function (context, req) {
     context.log('body is ' + JSON.stringify(req.body || {}));
     context.log('version is ' + req.query.version);
     context.log('website push ID is ' + req.query.websitePushID);
     const blobService = azure.createBlobService();
-    blobService.getBlobToText('indexedmessagesstatic','package.zip', (err, data) => {
+    let outputStream = new stream.Writable();
+    outputStream.contents = new Uint8Array(0);
+
+    //Override the write to store the value to our "contents"
+    // see https://stackoverflow.com/questions/43810082/azure-functions-nodejs-response-body-as-a-stream
+    outputStream._write = function (chunk, encoding, done) {
+        var curChunk = new Uint8Array(chunk);
+        var tmp = new Uint8Array(this.contents.byteLength + curChunk.byteLength);
+        tmp.set(this.contents, 0);
+        tmp.set(curChunk, this.contents.byteLength);
+        this.contents = tmp;
+        done();
+    };
+    
+    blobService.getBlobToStream('indexedmessagesstatic','package.zip', outputStream, (err, data) => {
 	if (err) {
 	    context.log('error');
 	    context.log(err);
@@ -17,11 +32,12 @@ module.exports = function (context, req) {
 	} else {
 	    context.log('Got data');
 		context.res = {
-		    body: data,
 		    status: 200,
 		    headers: {
 			'Content-Type': 'application/zip'
-		    }
+		    },
+		    isRaw: true,
+		    body: outputStream.contents
 		};
 	    context.done();
 	}
